d58964dfb15ffab4d4813b87d4e343c781c49cc7
Christopher Campbell <chris@launchbadge.com>
2020-07-31 14:27:52 -0700

    upgradeable

--- contracts/Hbar.sol
+++ contracts/Hbar.sol
@@ -2,14 +2,27 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
-import "@openzeppelin/upgrades/contracts/application/App.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
 contract Hbar is Initializable {
-    App private app;
-    address private owner;
+    using SafeMath for uint256;
+    bytes32 public constant OWNER = keccak256("OWNER");
+    bytes32 public constant SUPPLY_MANAGER = keccak256("SUPPLY_MANAGER");
+    bytes32 public constant ASSET_PROTECTION_MANAGER = keccak256(
+        "ASSET_PROTECTION_MANAGER"
+    );
 
-    function initialize(App _app) public initializer {
-        app = _app;
-        owner = msg.sender;
+    function init(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint32 tokenDecimal,
+        uint256 totalSupply,
+        address supplyManager,
+        address assetProtectionManager
+    ) public initializer {
+        // _setupRole(OWNER, owner);
+        // _setupRole(SUPPLY_MANAGER, supplyManager);
+        // _setupRole(ASSET_PROTECTION_MANAGER, assetProtectionManager);
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.5.0",
+      version: "0.6.8",
       docker: false,
       settings: {
        optimizer: {
