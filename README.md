01ce9006367821a8024d9e1b30709ef6b66807b4
Christopher Campbell <chris@launchbadge.com>
2020-07-31 12:22:34 -0700

    new base project

---
+++ README.md
@@ -0,0 +1,5 @@
+### Hbar Smart Contract
+
+ERC20 Externally Minted Token with Access Control
+
+Developed using OpenZeppelin CLI and the Truffle Suite
---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*",
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.6.8",
+      docker: false,
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  }
+}
