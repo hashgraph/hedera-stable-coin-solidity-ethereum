98b56032f8ab98e089b2993bba32edc8c1b357e9
Christopher Campbell <chris@launchbadge.com>
2020-07-31 11:11:22 -0700

    wip: claimable

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,11 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.7.0;
 
-contract Ownable {
-    address public owner;
+abstract contract Claimable {
     address internal newOwnerCandidate;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    modifier onlyNewOwner() {
+        require(
+            msg.sender == newOwnerCandidate,
+            "You must be chosen by the current owner to call this function."
+        );
+        _;
+    }
+
+    function claimOwnership() public virtual;
+}
+
+contract Ownable is Claimable {
+    address public owner;
 
     constructor() public {
         owner = msg.sender;
@@ -16,22 +32,14 @@ contract Ownable {
         _;
     }
 
-    modifier onlyNewOwner() {
-        require(
-            msg.sender == newOwnerCandidate,
-            "You must be chosen by the current owner to call this function."
-        );
-        _;
-    }
-
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner cannot be 0.");
         newOwnerCandidate = newOwner;
     }
 
-    function claimOwnership() public onlyNewOwner {
-        emit OwnershipTransferred(owner, msg.sender);
+    function claimOwnership() public override onlyNewOwner {
+        address _previousOwner = owner;
         owner = msg.sender;
-        newOwnerCandidate = address(0);
+        emit OwnershipTransferred(_previousOwner, owner);
     }
 }
