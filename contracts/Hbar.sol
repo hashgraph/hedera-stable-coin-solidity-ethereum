20f40e907cd672667b7f1f01191ec49e2f9c8552
Christopher Campbell <chris@launchbadge.com>
2020-08-03 12:06:41 -0700

    wip: ownable again

--- contracts/Hbar.sol
+++ contracts/Hbar.sol
@@ -2,15 +2,16 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract Hbar is Initializable, ContextUpgradeSafe, AccessControlUpgradeSafe, ERC20BurnableUpgradeSafe, ERC20PausableUpgradeSafe {
+contract Hbar is Initializable, ContextUpgradeSafe, OwnableUpgradeSafe, AccessControlUpgradeSafe, ERC20BurnableUpgradeSafe, ERC20PausableUpgradeSafe {
     using SafeMath for uint256;
-    bytes32 public constant OWNER = keccak256("OWNER");
     bytes32 public constant SUPPLY_MANAGER = keccak256("SUPPLY_MANAGER");
     bytes32 public constant ASSET_PROTECTION_MANAGER = keccak256("ASSET_PROTECTION_MANAGER");
+    address private _newOwner;
 
     modifier onlySupplyManager() {
         require(hasRole(SUPPLY_MANAGER, msg.sender), "Only a Supply Manager can call this function.");
@@ -19,6 +20,11 @@ contract Hbar is Initializable, ContextUpgradeSafe, AccessControlUpgradeSafe, ER
 
     modifier onlyAssetProtectionManager() {
         require(hasRole(ASSET_PROTECTION_MANAGER, msg.sender), "Only an Asset Protection Manager can call this function.");
+        _;
+    }
+
+    modifier onlyNewOwner() {
+        require(msg.sender == _newOwner, "Only owner designated new owner can call this function.");
         _;
     }
 
@@ -31,12 +37,12 @@ contract Hbar is Initializable, ContextUpgradeSafe, AccessControlUpgradeSafe, ER
         address assetProtectionManager
     ) public initializer {
         __Context_init_unchained();
+        __Ownable_init_unchained();
         __AccessControl_init_unchained();
         __ERC20_init_unchained(tokenName, tokenSymbol);
         __ERC20Burnable_init_unchained();
         __Pausable_init_unchained();
         __ERC20Pausable_init_unchained();
-        _setupRole(OWNER, msg.sender);
         _setupRole(SUPPLY_MANAGER, supplyManager);
         _setupRole(ASSET_PROTECTION_MANAGER, assetProtectionManager);
         _setupDecimals(tokenDecimal);
@@ -51,16 +57,27 @@ contract Hbar is Initializable, ContextUpgradeSafe, AccessControlUpgradeSafe, ER
         _burn(from, amount);
     }
 
-    function freeze() public onlyAssetProtectionManager {
+    function pause() public onlyAssetProtectionManager {
         _pause();
     }
 
-    function unfreeze() public onlyAssetProtectionManager {
+    function unpause() public onlyAssetProtectionManager {
         _unpause();
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20UpgradeSafe, ERC20PausableUpgradeSafe) {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner override(OwnableUpgradeSafe) {
+        require(newOwner != address(0), "Cannot set new owner to 0x00.");
+        _newOwner = newOwner;
+    }
+
+    function claimOwnership() public onlyNewOwner {
+        emit OwnershipTransferred(_owner, _newOwner);
+        _owner = _newOwner;
+        _newOwner = address(0);
     }
 
     uint256[50] private __gap;
