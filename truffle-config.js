fc6a1a36be637e413d22789762a86c00d5db257a
Christopher Campbell <chris@launchbadge.com>
2020-08-03 17:21:02 -0700

    modify network config

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,15 @@
 module.exports = {
   networks: {
-    development: {
+    ganache: {
      host: "127.0.0.1",
      port: 8545,
      network_id: "*",
     },
+    truffle: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*"
+    }
   },
 
   compilers: {
